c0ed00d0a2500f2b7cb6fdafce9ff83c9c5fdefa
Alexander Bochkarev <ptuchster@gmail.com>
2022-11-14 20:21:42 +0400

    Added minimal threshold to action inputs

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
-cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit
+cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
 
 if [ "${INPUT_USE_BUNDLER}" = "false" ]; then
   BUNDLE_EXEC=""
@@ -8,6 +8,18 @@ else
 fi
 
 echo '::group:: Preflight ...'
+
+${BUNDLE_EXEC}rubycritic -v || exit 1
+
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo "Seems like RubyCritic are not installed at your project :("
+fi
+echo '::endgroup::'
+
+echo '::group:: RubyCritic run'
+
+${BUNDLE_EXEC}rubycritic --mode-ci --format json --minimum-score ${INPUT_MIN_SCORE_THRESHOLD} -b master --maximum-decrease 0
 
 final_rc=$?
 echo '::endgroup::'
